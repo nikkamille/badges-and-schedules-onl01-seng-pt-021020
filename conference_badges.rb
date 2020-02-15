ab584da5bb0605c775a5755dff842a2c64f598d7
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-15 04:03:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(attendees)
   room_assignments = []
-  attendees.each_with_index {|name, index| room_assignments << "Hello, #{name}! You'll be assigned to "
+  attendees.each_with_index {|name, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}"
 end
 
 def printer
