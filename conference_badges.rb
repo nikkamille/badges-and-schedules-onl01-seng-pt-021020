ce560b9c77ea0f18c0026cf3871db948d1ebf1be
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-15 03:45:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator
+  badges = []
+  attendees.each {|name| badges << badge_maker}
+  badges
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
