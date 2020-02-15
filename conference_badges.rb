c429879eb335d8832eadcabf3207c9f8149ab700
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-15 03:54:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(names) #takes an array of names as an argument
   badges = []
-  attendees.each {|name| badges << badge_maker}
+  names.each {|name| badges << "Hello"}
   badges
 end
 
